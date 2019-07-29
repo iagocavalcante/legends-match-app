09266d50bd645caa0d49d9eb69a36d92f9814255
Iago Cavalcante <iagoangelimc@gmail.com>
2019-07-29 09:07:26 -0300

    :lipstick: ui: create profile screen
    this commit resolve #2. Build screen component based on profile screen made in adobe XD

---
+++ app/components/LMActionBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <ActionBar flat="true">
+    <FlexboxLayout alignContent="center" class="header">
+      <Label alignSelf="center" class="header-legend" text="LEGENDS"></Label>
+      <Label alignSelf="center" class="header-match" text="MATCH"></Label>
+    </FlexboxLayout>
+    <NavigationButton text="" android.systemIcon="ic_menu_back"/>
+  </ActionBar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  horizontal-align: center;
+  font-size: 20;
+  .header-legend {
+    font-weight: 300;
+    color: #be1965;
+  }
+  .header-match {
+    font-weight: bold;
+    color: #633280;
+  }
+}
+</style>
---
+++ app/components/LMChips.vue
@@ -0,0 +1,31 @@
+<template>
+  <Label class="chips" :text="text"></Label>
+</template>
+
+<script>
+export default {
+  name: 'LMChips',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.chips {
+  height: 26;
+  margin-left: 10;
+  border-radius: 13;
+  font-family: "Poppins";
+  background-color: #633280;
+  color: white;
+  text-align: center;
+  padding: 10;
+  margin-top: 10;
+  font-size: 14;
+  font-weight: 300;
+}
+</style>
